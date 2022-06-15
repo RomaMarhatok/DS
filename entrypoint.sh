32c86e44ea75edc83063e86bcf6d1b3f440d0229
roma <roma.marhatok@yandex.by>
2022-06-15 14:12:24 +0300

    add: modify entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Collect static files
-# echo "Collect static files"
-# python DjangoShop/manage.py collectstatic --noinput
+cd DjangoShop
+
+#Start Tests
+echo "Starting tests"
+poetry shell
+pytest -rP
 
 # Apply database migrations
 echo "Apply database migrations"
-python DjangoShop/manage.py migrate
+python manage.py migrate
 
 # Start server
 echo "Starting server"
-python DjangoShop/manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
