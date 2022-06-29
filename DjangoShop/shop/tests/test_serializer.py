3343424f85d3d803fa8650b90115130bd7b98001
roma <roma.marhatok@yandex.by>
2022-06-29 19:33:08 +0300

    add: new tests

--- DjangoShop/shop/tests/test_serializer.py
+++ DjangoShop/shop/tests/test_serializer.py
@@ -22,33 +22,35 @@ from shop.models.basket import Basket
 
 class TestProductSerializer:
     @pytest.mark.django_db
-    def test_product_serializer_from_json(self, faker_product_fixture):
-        data: dict = faker_product_fixture
-        serializer: ProductSerializer = ProductSerializer(data=data)
-        serializer.is_valid()
-        assert serializer.errors == {}
-        assert "slug" in serializer.data
-        assert "name" in serializer.data
-
-    @pytest.mark.django_db
-    def test_product_serializer_from_object(self, product_fixture, category_fixture):
+    def test_object_serialize(self, product_fixture, category_fixture):
         product: Product = product_fixture
         serializer: ProductSerializer = ProductSerializer(product)
         category: Category = category_fixture
         CategoryProduct.objects.create(product=product, category=category)
+
         assert "attributes" in serializer.data
         assert "category_product" in serializer.data
         assert isinstance(serializer.data["category_product"], list)
         assert isinstance(serializer.data["attributes"], list)
 
+    @pytest.mark.django_db
+    def test_serialize_json_without_slug(self, faker_product_fixture):
+        serializer: ProductSerializer = ProductSerializer(data=faker_product_fixture)
+        serializer.is_valid(raise_exception=True)
+
+        assert "name" in serializer.data
+        assert "slug" in serializer.data
+        assert "price" in serializer.data
+
 
 class TestCategoryProductSerializer:
     @pytest.mark.django_db
-    def test_category_product_serializer(self, category_porduct_fixture):
+    def test_object_serialize(self, category_porduct_fixture):
         category_product: CategoryProduct = category_porduct_fixture
         serializer: CategoryProductSerailizer = CategoryProductSerailizer(
             instance=category_product
         )
+
         assert "product" in serializer.data
         assert "category" in serializer.data
         assert isinstance(serializer.data["category"], OrderedDict)
@@ -57,7 +59,7 @@ class TestCategoryProductSerializer:
 
 class TestCategoryAttributeSerializer:
     @pytest.mark.django_db
-    def test_category_attribute_serializer(self, category_fixture, attribute_fixture):
+    def test_object_serialize(self, category_fixture, attribute_fixture):
         category: Category = category_fixture
         attribute: Attribute = attribute_fixture
         category_attribute: CategoryAttribute = CategoryAttribute.objects.create(
@@ -66,6 +68,7 @@ class TestCategoryAttributeSerializer:
         serializer: CategoryAttributeSerializer = CategoryAttributeSerializer(
             instance=category_attribute
         )
+
         assert "category" in serializer.data
         assert "attribute" in serializer.data
         assert isinstance(serializer.data["category"], OrderedDict)
@@ -77,6 +80,7 @@ class TestAttributeSerializer:
     def test_attribute_foreign_keys(self, attribute_fixture):
         attribute: Attribute = attribute_fixture
         serializer: AttributeSerializer = AttributeSerializer(instance=attribute)
+
         assert "product" in serializer.data
         assert isinstance(serializer.data["product"], OrderedDict)
 
@@ -86,25 +90,36 @@ class TestCategorySerializer:
     def test_category_unique_value(self, category_fixture):
         category: Category = category_fixture
         serializer: CategorySerializer = CategorySerializer(instance=category)
+
         assert "slug" in serializer.data
         assert "name" in serializer.data
         assert "category_product" in serializer.data
         assert isinstance(serializer.data["category_product"], list)
 
+    @pytest.mark.django_db
+    def test_serialize_json_without_slug(self, faker_category_fixture):
+        serializer: CategorySerializer = CategorySerializer(data=faker_category_fixture)
+        serializer.is_valid(raise_exception=True)
+
+        assert "slug" in serializer.data
+        assert "name" in serializer.data
+
 
 class TestOrderSerializer:
     @pytest.mark.django_db
-    def test_order_serializer(self, order_fixture):
+    def test_object_serialize(self, order_fixture):
         order: Order = order_fixture
         serializer: OrderSerialzier = OrderSerialzier(instance=order)
+
         assert "product" in serializer.data
         assert "user" in serializer.data
 
 
 class TestBasketSerializer:
     @pytest.mark.django_db
-    def test_basket_serializer(self, basket_fixture):
+    def test_object_serialize(self, basket_fixture):
         basket: Basket = basket_fixture
         serializer: BasketSerializer = BasketSerializer(instance=basket)
+
         assert "product" in serializer.data
         assert "user" in serializer.data
